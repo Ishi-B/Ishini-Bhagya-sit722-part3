b540b0e21dd370e7cc60e6c5e8b0afbd4f1ccf5d
Chethaka Lakshitha <chethaka.sl@gmail.com>
2024-09-15 12:42:26 +0530

    Triggering the GitAction

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://assessment_part_2_user:QJVGtCXlv9JULkLi34Ou7MCc4b9vcGvl@dpg-cripj5jv2p9s738mbqvg-a.singapore-postgres.render.com/assessment_part_2?sslmode=require')
--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "DATABASE_URL', 'postgresql://assessment_part_2_user:QJVGtCXlv9JULkLi34Ou7MCc4b9vcGvl@dpg-cripj5jv2p9s738mbqvg-a.singapore-postgres.render.com/assessment_part_2?sslmode=require" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
